0a37a09aef441940b115f677f1a2f8935715b355
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-02-03 18:22:17 +0200

    fix links

--- app/(overveiw)/projects/layout.jsx
+++ app/(overveiw)/projects/layout.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Template from "../../template";
 import {usePathname} from 'next/navigation';
+import Link from 'next/link'
 
 export default function RootLayout({ children }) {
   const location = usePathname();
@@ -8,11 +9,11 @@ export default function RootLayout({ children }) {
       <div style={{width:"100%", height:"100%",textAlign:"center"}}>
         <div style={{width:"100%", paddingTop:"60px", textAlign:"center"}}><h1>Projects</h1>
             <ul>
-                <li className='linkOfProject'><a href="/projects/1" aria-label='projects #1'>Project #1</a></li>
-                <li className='linkOfProject'><a href="/projects/2" aria-label='projects #2'>Project #2</a></li>
-                <li className='linkOfProject'><a href="/projects/3" aria-label='projects #3'>Project #3</a></li>
-                <li className='linkOfProject'><a href="/projects/4" aria-label='projects #4'>Project #4</a></li>
-                <li className='linkOfProject'><a href="/projects/5" aria-label='projects #5'>Project #5</a></li>
+                <li className='linkOfProject'><Link href="/projects/1" aria-label='projects #1'>Project #1</Link></li>
+                <li className='linkOfProject'><Link href="/projects/2" aria-label='projects #2'>Project #2</Link></li>
+                <li className='linkOfProject'><Link href="/projects/3" aria-label='projects #3'>Project #3</Link></li>
+                <li className='linkOfProject'><Link href="/projects/4" aria-label='projects #4'>Project #4</Link></li>
+                <li className='linkOfProject'><Link href="/projects/5" aria-label='projects #5'>Project #5</Link></li>
             </ul>
             </div>
             <Template key={location}>
--- app/(overveiw)/services/layout.jsx
+++ app/(overveiw)/services/layout.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Template from "../../template";
 import {usePathname} from 'next/navigation';
+import Link from 'next/link'
 
 export default function RootLayout({ children }) {
   const location = usePathname();
@@ -8,11 +9,11 @@ export default function RootLayout({ children }) {
       <div style={{width:"100%", height:"100%",textAlign:"center"}}>
         <div style={{width:"100%", paddingTop:"60px", textAlign:"center"}}><h1>Services</h1>
             <ul>
-                <li className='linkOfServices'><a href="/services/1" aria-label='services #1'>Service #1</a></li>
-                <li className='linkOfServices'><a href="/services/2" aria-label='services #2'>Service #2</a></li>
-                <li className='linkOfServices'><a href="/services/3" aria-label='services #3'>Service #3</a></li>
-                <li className='linkOfServices'><a href="/services/4" aria-label='services #4'>Service #4</a></li>
-                <li className='linkOfServices'><a href="/services/5" aria-label='services #5'>Service #5</a></li>
+                <li className='linkOfServices'><Link href="/services/1" aria-label='services #1'>Service #1</Link></li>
+                <li className='linkOfServices'><Link href="/services/2" aria-label='services #2'>Service #2</Link></li>
+                <li className='linkOfServices'><Link href="/services/3" aria-label='services #3'>Service #3</Link></li>
+                <li className='linkOfServices'><Link href="/services/4" aria-label='services #4'>Service #4</Link></li>
+                <li className='linkOfServices'><Link href="/services/5" aria-label='services #5'>Service #5</Link></li>
             </ul>
             </div>
             <Template key={location}>
