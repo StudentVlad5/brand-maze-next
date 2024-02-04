954003d39ecf80459f461a5fa46c4f6fd884f59d
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-02-04 12:55:10 +0200

    add method map

--- app/(overveiw)/projects/layout.jsx
+++ app/(overveiw)/projects/layout.jsx
@@ -1,19 +1,37 @@
 "use client"
 import Template from "../../template";
-import {usePathname} from 'next/navigation';
-import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function RootLayout({ children }) {
   const location = usePathname();
+  const data = [
+    {
+      name: "project 1",
+      id: 1,
+    },
+    {
+      name: "project 2",
+      id: 2,
+    },
+    {
+      name: "project 3",
+      id: 3,
+    },
+    {
+      name: "project 4",
+      id: 4,
+    },
+    {
+      name: "project 5",
+      id: 5,
+    },
+];
   return (
       <div style={{width:"100%", height:"100%",textAlign:"center"}}>
         <div style={{width:"100%", paddingTop:"60px", textAlign:"center"}}><h1>Projects</h1>
             <ul>
-                <li className='linkOfProject'><Link href="/projects/1" aria-label='projects #1'>Project #1</Link></li>
-                <li className='linkOfProject'><Link href="/projects/2" aria-label='projects #2'>Project #2</Link></li>
-                <li className='linkOfProject'><Link href="/projects/3" aria-label='projects #3'>Project #3</Link></li>
-                <li className='linkOfProject'><Link href="/projects/4" aria-label='projects #4'>Project #4</Link></li>
-                <li className='linkOfProject'><Link href="/projects/5" aria-label='projects #5'>Project #5</Link></li>
+                {data.map(it=><li key={it.id} className='linkOfProject'><Link href={'/projects/'+ it.id} aria-label={'project #' + it.id}>{'Project # ' + it.id}</Link></li>)}
             </ul>
             </div>
             <Template key={location}>
--- app/(overveiw)/services/layout.jsx
+++ app/(overveiw)/services/layout.jsx
@@ -5,15 +5,33 @@ import Link from 'next/link'
 
 export default function RootLayout({ children }) {
   const location = usePathname();
+  const data = [
+    {
+      name: "service 1",
+      id: 1,
+    },
+    {
+      name: "service 2",
+      id: 2,
+    },
+    {
+      name: "service 3",
+      id: 3,
+    },
+    {
+      name: "service 4",
+      id: 4,
+    },
+    {
+      name: "service 5",
+      id: 5,
+    },
+];
   return (
       <div style={{width:"100%", height:"100%",textAlign:"center"}}>
         <div style={{width:"100%", paddingTop:"60px", textAlign:"center"}}><h1>Services</h1>
             <ul>
-                <li className='linkOfServices'><Link href="/services/1" aria-label='services #1'>Service #1</Link></li>
-                <li className='linkOfServices'><Link href="/services/2" aria-label='services #2'>Service #2</Link></li>
-                <li className='linkOfServices'><Link href="/services/3" aria-label='services #3'>Service #3</Link></li>
-                <li className='linkOfServices'><Link href="/services/4" aria-label='services #4'>Service #4</Link></li>
-                <li className='linkOfServices'><Link href="/services/5" aria-label='services #5'>Service #5</Link></li>
+                {data.map(it=><li key={it.id} className='linkOfServices'><Link href={'/services/'+ it.id} aria-label={'service #' + it.id}>{'Service # ' + it.id}</Link></li>)}
             </ul>
             </div>
             <Template key={location}>
