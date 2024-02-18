5619579ece3ec4997bd927f09486cadcb002a38f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-02-18 11:19:19 +0200

    fix build

--- app/(overveiw)/contact/page.jsx
+++ app/(overveiw)/contact/page.jsx
@@ -1,20 +1,20 @@
 "use client"
 import  ClipLoaderSpinner  from '../../loading';
 import { Suspense } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function Contact() { 
     const router = usePathname();
-    const rout = useRouter();
-    function getRendomInt (count){
-        return Math.floor(Math.random()*count)
-    }
-    const random = getRendomInt(2);
-    if(random === 1){
-        throw new Error("Error on page Contact");
-    }
-    if(random !== 1){
+    // const rout = useRouter();
+    // function getRendomInt (count){
+    //     return Math.floor(Math.random()*count)
+    // }
+    // const random = getRendomInt(2);
+    // if(random === 1){
+    //     throw new Error("Error on page Contact");
+    // }
+    // if(random !== 1){
             // redirect('about/');
-            setTimeout(()=>{rout.push('about/')},2000);
-    }
+    //         setTimeout(()=>{rout.push('about/')},2000);
+    // }
     return <div style={{width:"100%", paddingTop:"60px", textAlign:"center"}}><Suspense key={router} fallback={<ClipLoaderSpinner/>}><h1>Contact</h1></Suspense></div> }
