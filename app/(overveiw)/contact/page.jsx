2830f978fcc0717617c384261832c045a50d1dce
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-02-18 13:44:48 +0200

    add redirect

--- app/(overveiw)/contact/page.jsx
+++ app/(overveiw)/contact/page.jsx
@@ -1,20 +1,19 @@
 "use client"
 import  ClipLoaderSpinner  from '../../loading';
 import { Suspense } from 'react';
-import { usePathname } from 'next/navigation';
+import { redirect, usePathname, useRouter } from 'next/navigation';
 
 export default function Contact() { 
     const router = usePathname();
-    // const rout = useRouter();
-    // function getRendomInt (count){
-    //     return Math.floor(Math.random()*count)
-    // }
-    // const random = getRendomInt(2);
-    // if(random === 1){
-    //     throw new Error("Error on page Contact");
-    // }
-    // if(random !== 1){
-    //         // redirect('about/');
-    //         setTimeout(()=>{rout.push('about/')},2000);
-    // }
+    const rout = useRouter();
+    function getRendomInt (count){
+        return Math.floor(Math.random()*count)
+    }
+    const random = getRendomInt(2);
+    if(random === 1){
+        redirect('/');
+    }
+    if(random !== 1){
+            // setTimeout(()=>{rout.push('/')},2000);
+    }
     return <div style={{width:"100%", paddingTop:"60px", textAlign:"center"}}><Suspense key={router} fallback={<ClipLoaderSpinner/>}><h1>Contact</h1></Suspense></div> }
