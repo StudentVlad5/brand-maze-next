1edcb2f06b412a7685aaa35f8f9f515e350364a2
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-02-18 13:23:13 +0200

    fix modal window

--- app/(overveiw)/(.)login/page.jsx
+++ app/(overveiw)/(.)login/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
-export default function Login() { 
-   
+export default function ModalLogin() { 
     return <div style={{ display: "flex", position:"absolute", width: "100vw", height: "100vh", backgroundColor:'#000', opacity:"0.5", top: "0", left:"0", justifyContent:"center", alignItems:"center"}}>
-    <h1 style={{color:"white"}}>Login Form STRONG Redirect</h1>
-</div>}
+        <h1 style={{color:"white"}}>Login Form SOFT  Redirect</h1>
+    </div>
+ }
--- app/(overveiw)/@auth/(.)login/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-export default function ModalLogin() { 
-    <div style={{ display: "flex", position:"absolute", width: "100vw", height: "100vh", backgroundColor:'#000', opacity:"0.5"}}>
-        <h1>Login Form</h1>
-    </div>
- }
