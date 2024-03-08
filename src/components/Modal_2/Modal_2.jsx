ae35f673677c0068bc452104755b0dbf52e66d06
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-03-08 18:16:28 +0200

    add modal_2

---
+++ src/components/Modal_2/Modal_2.jsx
@@ -0,0 +1,5 @@
+export const Modal = () => {
+    <div style={{ display: "flex", position:"absolute", width: "100vw", height: "100vh", backgroundColor:'#000', opacity:"0.5"}}>
+        <h1>Login Form</h1>
+    </div>
+}
