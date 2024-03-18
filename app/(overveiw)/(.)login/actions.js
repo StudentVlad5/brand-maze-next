411afd83cabe1315e1315410fa0e7f71a269a427
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-03-18 13:14:07 +0200

    soft login

--- app/(overveiw)/(.)login/actions.js
+++ app/(overveiw)/(.)login/actions.js
@@ -1,6 +1,6 @@
 "use server";
 export async function createUser(prevState, formData) {
   // ...
-  console.log("BLA_BLA");
-  return res.status(200).json({ message: "Please enter a valid email" });
+  return ({ message: "Please enter a valid email"})
 }
+
